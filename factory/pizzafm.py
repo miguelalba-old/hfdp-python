a311a91f1ea95b8c1646eb7eb97f9ed1d6e0b682
Miguel Alba <miguelalba89@gmail.com>
2014-03-25 10:40:18 +0100

    Factory pattern: Pizza factory method

--- factory/pizzafm.py
+++ factory/pizzafm.py
@@ -1,6 +1,16 @@
-from abc import ABCMeta, abstractmethod
-from __builtin__ import super
+"""
+Pizza factory method
 
+Author: m1ge7
+Date: 2014/03/25
+"""
+
+from abc import ABCMeta, abstractmethod
+
+
+###############################################################################
+# Pizzas
+###############################################################################
 
 class Pizza:
     __metaclass__ = ABCMeta
@@ -32,6 +42,134 @@ class Pizza:
     def get_name(self):
         return self._name
 
+    def __str__(self):
+        display = ""
+        display += "---- " + self._name + " ----\n"
+        display += self._dough + "\n"
+        display += self._sauce + "\n"
+        display += "\n".join(self._toppings)
+        return display
+
+
+class NYStyleCheesePizza(Pizza):
+    
+    def __init__(self):
+        super(NYStyleCheesePizza, self).__init__()
+        self._name = "NY Style Sauce and Cheese Pizza"
+        self._dough = "Thin Crust Dough"
+        self._sauce = "Marinara Sauce"
+        self._toppings.append("Grated Reggiano Cheese")
+
+
+class NYStylePepperoniPizza(Pizza):
+
+    def __init__(self):
+        super(NYStylePepperoniPizza, self).__init__()
+        self._name = "NY Style Pepperoni Pizza"
+        self._dough = "Thin Crust Dough"
+        self._sauce = "Marinara Sauce"
+        self._toppings.append("Grated Reggiano Cheese")
+        self._toppings.append("Sliced Pepperoni")
+        self._toppings.append("Garlic")
+        self._toppings.append("Onion")
+        self._toppings.append("Mushrooms")
+        self._toppings.append("Red Pepper")
+
+
+class NYStyleClamPizza(Pizza):
+
+    def __init__(self):
+        super(NYStyleClamPizza, self).__init__()
+        self._name = "NY Style Clam Pizza"
+        self._dough = "Thin Crust Dough"
+        self._sauce = "Marinara Sauce"
+        self._toppings.append("Grated Reggiano Cheese")
+        self._toppings.append("Fresh Clams from Long Island Sound")
+
+
+class NYStyleVeggiePizza(Pizza):
+
+    def __init__(self):
+        super(NYStyleVeggiePizza, self).__init__()
+        self._name = "NY Style Veggie Pizza"
+        self._dough = "Thin Crust Dough"
+        self._sauce = "Marinara Sauce"
+
+        self._toppings.append("Grated Reggiano Cheese")
+        self._toppings.append("Garlic")
+        self._toppings.append("Onion")
+        self._toppings.append("Mushrooms")
+        self._toppings.append("Red Pepper")
+
+
+class ChicagoStyleCheesePizza(Pizza):
+
+    def __init__(self):
+        super(ChicagoStyleCheesePizza, self).__init__()
+        self._name = "Chicago Style Deep Dish Cheese Pizza"
+        self._dough = "Extra Thick Crust Dough"
+        self._sauce = "Plum Tomato Sauce"
+        self._toppings.append("Shredded Mozzarella Cheese")
+
+
+    def cut(self):
+        print("Cutting the pizza into square slices")
+
+
+class ChicagoStylePepperoniPizza(Pizza):
+    
+    def __init__(self):
+        super(ChicagoStylePepperoniPizza, self).__init__()
+        self._name = "Chicago Style Pepperoni Pizza"
+        self._dough = "Extra Thick Crust Dough"
+        self._sauce = "Plum Tomato Sauce"
+
+        self._toppings.append("Shredded Mozzarella Cheese");
+        self._toppings.append("Black Olives");
+        self._toppings.append("Spinach");
+        self._toppings.append("Eggplant");
+        self._toppings.append("Sliced Pepperoni");
+ 
+    def cut(self):
+        print("Cutting the pizza into square slices")
+    
+
+class ChicagoStyleClamPizza(Pizza):
+    
+    def __init__(self):
+        super(ChicagoStyleClamPizza, self).__init__()
+        self._name = "Chicago Style Clam Pizza"
+        self._dough = "Extra Thick Crust Dough"
+        self._sauce = "Plum Tomato Sauce"
+        
+        self._toppings.append("Shredded Mozzarella Cheese")
+        self._toppings.append("Frozen Clams from Chesapeake Bay")
+        
+
+    def cut(self):
+        print("Cutting the pizza into square slices")
+
+
+class ChicagoStyleVeggiePizza(Pizza):
+    
+    def __init__(self):
+        super(ChicagoStyleVeggiePizza, self).__init__()
+        self._name = "Chicago Deep Dish Veggie Pizza"
+        self._dough = "Extra Thick Crust Dough"
+        self._sauce = "Plum Tomato Sauce"
+        
+        self._toppings.append("Shredded Mozzarella Cheese");
+        self._toppings.append("Black Olives");
+        self._toppings.append("Spinach");
+        self._toppings.append("Eggplant");
+
+    def cut(self):
+        print("Cutting the pizza into square slices")
+
+
+###############################################################################
+# Pizza stores
+###############################################################################
 
 class PizzaStore:
     __metaclass__ = ABCMeta
@@ -67,57 +205,6 @@ class NYPizzaStore(PizzaStore):
             return None
 
 
-class NYStyleCheesePizza(Pizza):
-    
-    def __init__(self):
-        super(NYStyleCheesePizza, self).__init__()
-        self._name = "NY Style Sauce and Cheese Pizza"
-        self._dough = "Thin Crust Dough"
-        self._sauce = "Marinara Sauce"
-        self._toppings.append("Grated Reggiano Cheese")
-
-
-class NYStyleVeggiePizza(Pizza):
-
-    def __init__(self):
-        super(NYStyleVeggiePizza, self).__init__()
-        self._name = "NY Style Veggie Pizza"
-        self._dough = "Thin Crust Dough"
-        self._sauce = "Marinara Sauce"
-
-        self._toppings.append("Grated Reggiano Cheese")
-        self._toppings.append("Garlic")
-        self._toppings.append("Onion")
-        self._toppings.append("Mushrooms")
-        self._toppings.append("Red Pepper")
-
-
-class NYStyleClamPizza(Pizza):
-
-    def __init__(self):
-        super(NYStyleClamPizza, self).__init__()
-        self._name = "NY Style Clam Pizza"
-        self._dough = "Thin Crust Dough"
-        self._sauce = "Marinara Sauce"
-        self._toppings.append("Grated Reggiano Cheese")
-        self._toppings.append("Fresh Clams from Long Island Sound")
-
-
-class NYStylePepperoniPizza(Pizza):
-
-    def __init__(self):
-        super(NYStylePepperoniPizza, self).__init__()
-        self._name = "NY Style Pepperoni Pizza"
-        self._dough = "Thin Crust Dough"
-        self._sauce = "Marinara Sauce"
-        self._toppings.append("Grated Reggiano Cheese")
-        self._toppings.append("Sliced Pepperoni")
-        self._toppings.append("Garlic")
-        self._toppings.append("Onion")
-        self._toppings.append("Mushrooms")
-        self._toppings.append("Red Pepper")
-
-
 class ChicagoPizzaStore(PizzaStore):
 
     def create_pizza(self, item):
@@ -133,70 +220,9 @@ class ChicagoPizzaStore(PizzaStore):
             return None
 
 
-class ChicagoStyleCheesePizza(Pizza):
-
-    def __init__(self):
-        super(ChicagoStyleCheesePizza, self).__init__()
-        self._name = "Chicago Style Deep Dish Cheese Pizza"
-        self._dough = "Extra Thick Crust Dough"
-        self._sauce = "Plum Tomato Sauce"
-        self._toppings.append("Shredded Mozzarella Cheese")
-
-
-    def cut(self):
-        print("Cutting the pizza into square slices")
-
-
-class ChicagoStyleVeggiePizza(Pizza):
-    
-    def __init__(self):
-        super(ChicagoStyleVeggiePizza, self).__init__()
-        self._name = "Chicago Deep Dish Veggie Pizza"
-        self._dough = "Extra Thick Crust Dough"
-        self._sauce = "Plum Tomato Sauce"
-        
-        self._toppings.append("Shredded Mozzarella Cheese");
-        self._toppings.append("Black Olives");
-        self._toppings.append("Spinach");
-        self._toppings.append("Eggplant");
-
-    def cut(self):
-        print("Cutting the pizza into square slices")
-
-
-class ChicagoStyleClamPizza(Pizza):
-    
-    def __init__(self):
-        super(ChicagoStyleClamPizza, self).__init__()
-        self._name = "Chicago Style Clam Pizza"
-        self._dough = "Extra Thick Crust Dough"
-        self._sauce = "Plum Tomato Sauce"
-        
-        self._toppings.append("Shredded Mozzarella Cheese")
-        self._toppings.append("Frozen Clams from Chesapeake Bay")
-        
-
-    def cut(self):
-        print("Cutting the pizza into square slices")
-
-
-class ChicagoStylePepperoniPizza(Pizza):
-    
-    def __init__(self):
-        super(ChicagoStylePepperoniPizza, self).__init__()
-        self._name = "Chicago Style Pepperoni Pizza"
-        self._dough = "Extra Thick Crust Dough"
-        self._sauce = "Plum Tomato Sauce"
-
-        self._toppings.append("Shredded Mozzarella Cheese");
-        self._toppings.append("Black Olives");
-        self._toppings.append("Spinach");
-        self._toppings.append("Eggplant");
-        self._toppings.append("Sliced Pepperoni");
- 
-    def cut(self):
-        print("Cutting the pizza into square slices")
-    
+###############################################################################
+# Simulation
+###############################################################################
 
 if __name__ == '__main__':
     nyStore = NYPizzaStore()
